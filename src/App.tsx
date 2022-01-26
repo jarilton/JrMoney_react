a49e75212fee187a6a069d9bdb3eb021e7120a29
Jarilton <jariltonpereira02@hotmail.com>
2022-01-26 16:57:29 -0300

    Styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Bem vindo ao JR Money</h1>
+      <Title>Bem vindo ao JR Money</Title>
     </div>
   );
 }
